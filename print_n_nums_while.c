89a8db26d00d07575f9217dcec2cbe83e993a434
iampreetiranjan <46985922+iampreetiranjan@users.noreply.github.com>
2025-11-08 13:06:59 +0530

    Create print_n_nums_while.c

---
+++ print_n_nums_while.c
@@ -0,0 +1,14 @@
+// Print first n Natural Numbers 
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter n: ");
+    scanf("%d", &n);
+    int i = 1;
+    while (i <= 10) {
+        printf("%d ", i);
+        i++;
+    }
+    return 0;
+}
